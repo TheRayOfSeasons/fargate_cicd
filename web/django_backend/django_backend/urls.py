a745e4fa8713ed0aaecd3058852256ee799aa8d1
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2020-12-08 11:52:58 +0800

    Add: Static and Media urls in root router file

--- web/django_backend/django_backend/urls.py
+++ web/django_backend/django_backend/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
@@ -22,3 +23,8 @@ urlpatterns = [
     path('', include('todos.urls', namespace='todos')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static.static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static.static(
+    settings.STATIC_URL, document_root=settings.STATIC_ROOT)
