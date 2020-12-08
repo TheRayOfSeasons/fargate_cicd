86340383250ea478124206485d7e5123eb90c9a5
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2020-12-08 18:17:00 +0800

    Add: Explicit VPC for Cluster

--- fargate_cicd/cluster.py
+++ fargate_cicd/cluster.py
@@ -1,4 +1,5 @@
 from aws_cdk import core
+from aws_cdk import aws_ec2 as ec2
 from aws_cdk import aws_ecr as ecr
 from aws_cdk import aws_ecs as ecs
 
@@ -9,8 +10,9 @@ class Cluster(core.Construct):
     """
 
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs):
-        super().__init__(scope, construct_id, **kwargs)
-        self.ecsCluster = ecs.Cluster(self, 'sample-ecs-cluster')
+        super().__init__(scope, construct_id)
+        vpc = ec2.Vpc(self, 'sample-fargate-vpc', max_azs=2)
+        self.ecsCluster = ecs.Cluster(self, 'sample-ecs-cluster', vpc=vpc)
         self.output()
 
     def output(self):
